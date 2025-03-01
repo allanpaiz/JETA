418906a09378568acca2279737cb5e231bfb3566
Allan Paiz - Win <allan.paiz0@gmail.com>
2025-02-28 23:15:26 -0500

    initial work of backend and UI merge

---
+++ src/main/java/com/tuneup/TODO/UserType.java
@@ -0,0 +1,21 @@
+package com.tuneup;
+
+public enum UserType {
+    TEACHER("Teacher"),
+    STUDENT("Student");
+
+    private final String displayName;
+
+    UserType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
