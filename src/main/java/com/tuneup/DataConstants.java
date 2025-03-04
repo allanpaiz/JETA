e20b387b3df2b719f94502103e23f740b7b508dc
Terdooachu <Tachu@email.sc.edu>
2025-03-04 12:26:08 -0500

    removed class files

--- src/main/java/com/tuneup/DataConstants.java
+++
@@ -1,7 +0,0 @@
-package com.tuneup;
-
-public interface DataConstants {
-    String USERS_FILE = "json/users.json"; // Update this path to the correct location of your users.json file
-    String LESSONS_FILE = "json/lessons.json"; // Update this path to the correct location of your lessons.json file
-    String SONGS_FOLDER = "songs/";
-}
--- src/main/java/com/tuneup/ExperienceLevel.java
+++
@@ -1,7 +0,0 @@
-package com.tuneup;
-
-public enum ExperienceLevel {
-    BEGINNER,
-    INTERMEDIATE,
-    ADVANCED
-}
