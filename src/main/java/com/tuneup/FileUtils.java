95f056a0e51dce86e44a6853e1e7b9de374e6caa
Allan Paiz - Win <allan.paiz0@gmail.com>
2025-03-23 21:15:41 -0400

    Migrated main() and other small changes for issues #39, #85, #80

--- src/main/java/com/tuneup/FileUtils.java
+++ src/main/java/com/tuneup/FileUtils.java
@@ -2,7 +2,17 @@ package com.tuneup;
 
 import java.io.File;
 
+/**
+ * Utility class
+ * 
+ * @author edwinjwood
+ * @author allanpaiz - javadoc
+ */
 public class FileUtils {
+
+    /**
+     * Ensures that the json folder exists, otherwise creates it
+     */
     public static void ensureDataFolderExists() {
         File jsonFolder = new File("json");
         if (!jsonFolder.exists()) {
